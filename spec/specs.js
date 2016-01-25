6917821b2ab3db4baadb115c1f9173b98428dc7e
brad pizzimenti and jared beckler <jared.beckler@gmail.com>
2016-01-25 11:07:18 -0800

    add failing spec for display header

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,9 @@ describe("Place", function(){
     expect(testPlace.placeLocation).to.equal("Paris")
     expect(testPlace.yearVisited).to.equal("2015")
   });
+
+  it("creates a method that concats the place name and year visited", function(){
+    var testPlace = new Place("Empire State Building","2011");
+    expect(testPlace.locDate()).to.equal("Empire State Building 2011");
+  });
 });
