631177445d36cf1ed333eab203199737cb046dad
brad pizzimenti and jared beckler <jared.beckler@gmail.com>
2016-01-25 11:57:30 -0800

    add form fuctionality with appending to list

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,21 @@ var Place = function(placeName,placeLocation,yearVisited) {
 Place.prototype.locDate = function(){
   return this.placeName + " " + this.yearVisited;
 }
+
+
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedPlaceName = $('input#new-place-name').val();
+    var inputtedPlaceLocation = $('input#location').val();
+    var inputtedYearVisited = $('input#year-visited').val();
+    var newPlace = new Place(inputtedPlaceName,inputtedPlaceLocation,inputtedYearVisited);
+
+    $('ul#places').append('<li><span class="place">' + inputtedPlaceName + '</span></li>');
+// debugger;
+    $("input#new-place-name").val("");
+    $("input#location").val("");
+    $("input#year-visited").val("");
+  });
+});
